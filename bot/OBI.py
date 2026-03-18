c921bfecefe3155725251b82d3e882ff64d915fe
Finn <lingjunf8@gmail.com>
2026-03-18 22:25:16 +0800

    Update OBI

--- bot/OBI.py
+++ bot/OBI.py
@@ -1,4 +1,35 @@
+import sys
+from pathlib import Path
+
+# 1. Get the current script's directory (/bot)
+current_dir = Path(__file__).resolve().parent
+
+# 2. Go UP one level to the parent folder (/Project_Parent)
+parent_dir = current_dir.parent
+
+# 3. Add the 'database' folder to Python's search path
+database_dir = parent_dir / "database"
+sys.path.append(str(database_dir))
+
+# 4. Now import your file
+from Binance_fetcher import BinanceDataFetcher
 import numpy as np
 import pandas as pd
+import pandas_ta as ta
 import time
-import time
+
+Holding = False
+
+fetcher = BinanceDataFetcher()
+recent_df = fetcher.fetch_recent_klines(symbol="BTCUSDT", interval="1m", limit=7)  #获取最新的10条1分钟K线数据
+
+mean_Buy = recent_df['buy_volume'].mean()
+mean_Sell = recent_df['sell_volume'].mean()
+
+OBI = (mean_Buy - mean_Sell)/(mean_Buy + mean_Sell)
+
+recent_df['RSI_7'] = ta.rsi(recent_df['close'], length=7)
+current_rsi = recent_df['RSI_7'].iloc[-1]
+
+if OBI>0.75 & current_rsi<60 :
+    print('Buy')
